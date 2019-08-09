277cbebbc13041032e04532d039b204e5bf28a46
Joshua Read <joshua.gray.read@gmail.com>
2019-08-09 15:21:07 +0200

    cookies

---
+++ cookies.php
@@ -0,0 +1,9 @@
+<?php
+/*
+$exp = time()+86400; //86400 is the amount of seconds in a 24 hour period
+setcookie("name", "Alex", $exp);
+*/
+// setcookie('age', 19, $expire);
+echo $_COOKIE("name");
+	
+?>

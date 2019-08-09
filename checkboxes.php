af207e92aeb58e2796dbaa2e33d060dafe895bbd
Joshua Read <joshua.gray.read@gmail.com>
2019-08-09 15:21:05 +0200

    cookies

---
+++ checkboxes.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+<form action="checkboxes-process.php" method="post">
+	<p><input type="checkbox" name="agree" value="true" /> I agree</p>
+	<p><input type="submit" value="Register" /></p>
+</form>
+*/
+?>
+<form action="checkboxes-process.php" method="post">
+	<p><input type="radio" name="language" value="PHP" /> PHP</p>
+	<p><input type="radio" name="language" value="C" /> C</p>
+	<p><input type="radio" name="language" value="BASIC" /> BASIC</p>
+	<p><input type="submit" value="Submit" /></p>
+</form>

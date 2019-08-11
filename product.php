8f145779ef412c94b390b8c4a2beae6e214bb4d8
Joshua Read <joshua.gray.read@gmail.com>
2019-08-11 14:55:37 +0200

    oop intro

---
+++ product.php
@@ -0,0 +1,20 @@
+<?php
+
+
+/**
+* Class Product
+*/
+class Product {
+	public $type;
+
+	public function setProductType($value) {
+		$this->type = $value;
+	}
+	
+	public function getProductType() {
+		return $this->type;
+	}
+}
+
+	
+?>

638c4431b4cabc87bc1870c8cb796eaf22638ed7
Joshua Read <joshua.gray.read@gmail.com>
2019-08-11 13:01:52 +0200

    functions

--- functions.php
+++ functions.php
@@ -1,5 +1,24 @@
 <?php
+
+echo '<div>';	
 	
-$name = strlen('Alex');
-echo 'Your name has ' . $name . ' characters.';
+$number = 1000000;
+$number--;
+
+$number_formatted = number_format($number, 0);
+echo 'You have &pound;' . $number_formatted;	
+
+echo '</div><div>';
+	
+if (isset($_POST['text'])) {
+	echo nl2br(htmlentities($_POST['text']));
+}
+
+echo '</div>';
+	
 ?>
+
+<form action="" method="post">
+	<textarea name="text"></textarea>
+	<input type="submit" />
+</form>
